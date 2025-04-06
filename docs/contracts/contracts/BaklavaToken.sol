dec2c6da1a516033fef225de8cfe14163cd729db
sayang44Malik <okulistik23@gmail.com>
2025-04-07 00:07:19 +0300

    Add BaklavaToken.sol contract with fixed supply

---
+++ docs/contracts/contracts/BaklavaToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BaklavaToken is ERC20, Ownable {
+    constructor() ERC20("Baklava Coin", "bKLV") {
+        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+    }
+
+    // Mint fonksiyonu kapalıdır. Ek basım yapılamaz.
+}
